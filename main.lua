37cbd41ea3e49b7b51315f4eb3d44f2d0fe0e8e2
IcyLava <icylavah@gmail.com>
2023-05-10 03:56:33 +0300

    Done?

--- main.lua
+++ main.lua
@@ -5,13 +5,22 @@ local l = G.line
 local M = math
 local s = M.sin
 local p = 0
+local T = L.timer
+local U = T.getTime
+local S = U()
 L.draw = function()
-  local t = L.timer.getTime()
+  local t = U()
+  G.print('score: ' .. M.floor(p))
+  if t - S > 30 then
+    return 
+  end
   local x = 400 + 400 * s(t + 2 * s(t + 3 * s(t)))
   local y = 300 + 300 * s(t + 3 * s(t + 5 * s(t)))
   local X = 400 + 400 * s(t + 4 * s(t + 4 * s(t)))
   local Y = 300 + 300 * s(t + 3 * s(t + 2 * s(t)))
   local o = V(x - X, y - Y)
   o.length = 1e4
-  return l(x + o.x, y + o.y, x - o.x, y - o.y)
+  l(x + o.x, y + o.y, x - o.x, y - o.y)
+  local d = 1e-4 * V(-o.y, o.x)
+  p = p + (T.getDelta() * M.max(0, 200 - M.abs(d:dot(V(x, y) - V(L.mouse.getPosition())))))
 end
--- main.moon
+++ main.moon
@@ -5,8 +5,13 @@ l=G.line
 M=math
 s=M.sin
 p=0
+T=L.timer
+U=T.getTime
+S=U!
 L.draw=->
-	t=L.timer.getTime!
+	t=U!
+	G.print 'score: '..M.floor p
+	return if t-S>30
 	x=400+400*s t+2*s t+3*s t
 	y=300+300*s t+3*s t+5*s t
 	X=400+400*s t+4*s t+4*s t
@@ -14,6 +19,5 @@ L.draw=->
 	o=V x-X,y-Y
 	o.length=1e4
 	l x+o.x,y+o.y,x-o.x,y-o.y
-	-- m=L.mouse.getY!
-	-- p+=.1*M.max 0,200-M.abs y-m
-	-- G.print M.floor p
+	d=1e-4*V -o.y,o.x
+	p+=T.getDelta!*M.max 0,200-M.abs d\dot(V(x,y)-V L.mouse.getPosition!)
